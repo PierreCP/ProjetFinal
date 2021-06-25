4e975e7838076c9d69f03fbbdc5d4f868ee2f124
gmorisse <uillaume.morisset1@gmail.com>
2021-06-25 14:47:52 +0200

    ajout des servies auth et access

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { AccessService } from '../access.service';
 
 @Component({
   selector: 'app-connexion',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnexionComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+  MsgErr: any;
+
+  constructor(private http: HttpClient, private route: Router, public authService: AuthService, private access: AccessService) { }
 
   ngOnInit(): void {
+  }
+
+  connexion(u: any): void{
+    this.http.post(this.access.getBackURL() + 'login', u).subscribe({
+      next: (data)=> {this.user = data; if (this.user.id != null) { this.authService.setUserInLocalStorage(this.user) ; this.route.navigateByUrl('memoprive');} else{
+        this.MsgErr = 'Indentifiant ou mot de passe incorrecte !'
+      }},
+      error: (err)=> {}
+    })
   }
 
 }
