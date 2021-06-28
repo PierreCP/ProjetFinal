f591898be62d21bcd32f7e756055bedb96f814f7
gmorisse <uillaume.morisset1@gmail.com>
2021-06-28 10:26:30 +0200

    Add menu-consommateur container

---
+++ src/app/menu-consommateur/menu-consommateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-consommateur',
+  templateUrl: './menu-consommateur.component.html',
+  styleUrls: ['./menu-consommateur.component.css']
+})
+export class MenuConsommateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
