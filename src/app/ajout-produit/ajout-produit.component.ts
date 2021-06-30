e304823b6083a7f0a44669e30ff4ed9a369cf12c
GeoffreyALB <g.albert13180@gmail.com>
2021-06-30 16:08:56 +0200

    commit on prod etc

---
+++ src/app/ajout-produit/ajout-produit.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { MatDialogRef } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-ajout-produit',
+  templateUrl: './ajout-produit.component.html',
+  styleUrls: ['./ajout-produit.component.css']
+})
+export class AjoutProduitComponent implements OnInit {
+
+  constructor(private http: HttpClient, private authService: AuthService, private route : Router, private dialogRef: MatDialogRef<AjoutProduitComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  ajoutProduit(m: any): void {
+
+    this.http.put('http://localhost:8082/person/', + this.authService.getUserInLocalStorage().id + '/produit/' /*+ produit + '/' + quant*/).subscribe({
+      next: (data)=> {
+        this.dialogRef.close()
+      /*Pour rafraichir DIRECTEMENT
+      this.routerouteReuseStrategy.shouldReuseRoute= () => false;
+      this.route.onSameUrlNavigation='reload'; 
+      this.route.navigateByUrl('menu-prive');*/
+      },
+        //this.route.navigateByUrl('memo-prive')},
+      error: (err) => {
+        console.log(err)}
+    });
+  }
+
+}
--- src/app/etal/etal.component.ts
+++ src/app/etal/etal.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AccessService } from '../access.service';
+import { AjoutProduitComponent } from '../ajout-produit/ajout-produit.component';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-etal',
@@ -8,8 +13,12 @@ import { Router } from '@angular/router';
 })
 export class EtalComponent implements OnInit {
 
-  constructor(private route: Router) { }
+  constructor(private http: HttpClient, private authService: AuthService, private route: Router, private access: AccessService) { }
 
+  liste: any;
+  liste2: any;
+  user: any;
+  MsgErr: any;
   ngOnInit(): void {
   }
 
@@ -36,4 +45,32 @@ export class EtalComponent implements OnInit {
     this.route.navigateByUrl('etal');
   }
 
+  retourMenuProd(): void {
+    this.route.navigateByUrl('menu-prod');
+  }
+
+  ajoutProduit(m: any): void {
+    this.user = this.authService.getUserInLocalStorage();
+    this.http.get('http://localhost:8082/person/' + this.user.id + '/produit/' + m.name + '/' + m.quantite + '/' + m.prix + '/' + m.description).subscribe({
+      next: (data) => { this.liste2 = data },
+      error: (err) => { console.log(err) }
+    })
+    this.MsgErr = 'Ajout du produit impossible.'
+  }
+
+  modifierProduit(m: any): void {
+    console.log('Ici on débute');
+    this.user=this.authService.getUserInLocalStorage();
+    this.http.get('http://localhost:8082/person/' + '/produit/' + this.user.id + '/' + m.name + '/' + m.quantite + '/' + m.prix + '/' + m.description).subscribe({
+      next: (data) => { this.liste2 = data},
+      error: (err) => { console.log(err) }
+    })
+          this.MsgErr = 'Ajout du produit impossible.'
+        }
+
+
 }
+
+
+
+
